ae8c795d2af94fd5242569c337eea44e5b9668fe
Strallia Chao <134454347+strallia@users.noreply.github.com>
2025-02-05 19:15:53 -0800

    feat: add dropdown to create expenses, events, or groups

--- src/components/Home/NavigationWrapper.jsx
+++ src/components/Home/NavigationWrapper.jsx
@@ -1,30 +1,65 @@
 import Bell from '@/assets/bell.svg';
 import User from '@/assets/user.svg';
-// import Calendar from '@/assets/calendar.svg';
+import Calendar from '@/assets/calendar.svg';
 import DollarSign from '@/assets/dollar-sign.svg';
 import Home from '@/assets/home.svg';
-// import MapPin from '@/assets/map-pin.svg';
 import PlusSquare from '@/assets/plus-square.svg';
-import Settings from '@/assets/settings.svg';
-// import Users from '@/assets/users.svg';
+import Users from '@/assets/users.svg';
+import Writing from '@/assets/writing.svg';
+import AddSubtract from '@/assets/add-subtract.svg';
+import { useState } from 'react';
 
-const NavButton = ({ image, imageAlt, styles }) => {
+const NavButton = ({ image, imageAlt, styles, onClick, text }) => {
   return (
     <button
-      className={`${styles} hover:bg-gray-100 hover:cursor-pointer hover:rounded-2xl pr-2.5 pl-2.5 h-min`}
+      className={`hover:bg-gray-100 hover:cursor-pointer hover:rounded-2xl px-2.5 h-min flex justify-center ${styles}`}
+      onClick={onClick}
     >
       <img src={image} alt={imageAlt} />
+      {text}
     </button>
   );
 };
 
 const NavigationWrapper = ({ children }) => {
+  const [showCreateItemDropdown, setShowCreateItemDropdown] = useState(false);
+
+  const toggleCreateItemDropdown = () => {
+    setShowCreateItemDropdown((prev) => !prev);
+  };
+
   return (
     <div className="grid h-full grid-rows-[50px_1fr_50px]">
-      <nav className="flex gap-5 p-2.5 bg-gray-200 items-center">
+      <nav className="flex gap-5 p-2.5 bg-gray-200 items-center relative">
         <NavButton image={User} imageAlt="profile" styles="mr-auto" />
         <NavButton image={Bell} imageAlt="reminders" />
-        <NavButton image={Settings} imageAlt="settings" />
+        <NavButton
+          image={Writing}
+          imageAlt="create expense, event, or group"
+          onClick={toggleCreateItemDropdown}
+        />
+        {showCreateItemDropdown ? (
+          <div className="absolute right-3 top-[120%] grid gap-1">
+            <NavButton
+              image={AddSubtract}
+              imageAlt="add expense"
+              text="Add Expense"
+              styles="py-[12px] px-[16px]"
+            />
+            <NavButton
+              image={Calendar}
+              imageAlt="create event"
+              text="Create Event"
+              styles="py-[12px] px-[16px]"
+            />
+            <NavButton
+              image={Users}
+              imageAlt="create group"
+              text="Create Group"
+              styles="py-[12px] px-[16px]"
+            />
+          </div>
+        ) : null}
       </nav>
       <main className="pt-2.5 pb-2.5 pr-5 pl-5">{children}</main>
       <nav className="flex gap-5 p-2.5 justify-center bg-gray-200 items-center">
