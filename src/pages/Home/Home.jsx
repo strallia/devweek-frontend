b159f242295db7696028d2fbf995de07decd2bf6
Strallia Chao <134454347+strallia@users.noreply.github.com>
2025-02-07 17:02:02 -0800

    chore: move mock data to utils

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,6 @@
 import NavigationWrapper from '@/layouts/NavigationWrapper';
 import List from './List';
-
-const events = [
-  {
-    groupName: 'Group Name',
-    eventName: 'Event Name',
-    date: new Date(),
-    location: 'san jose, CA',
-  },
-  {
-    groupName: 'Group Name',
-    eventName: 'Event Name',
-    date: new Date(),
-    location: 'san jose, CA',
-  },
-];
-
-const groups = [
-  {
-    groupName: 'Group Name',
-    users: ['Sam Johnson', 'Josie Rein', 'Jane Doe', 'Sellie Nosh'],
-    description: 'description',
-  },
-  {
-    groupName: 'Group Name',
-    users: ['Sam Johnson', 'Josie Rein', 'Jane Doe'],
-    description: 'description',
-  },
-];
+import { events, groups } from '@/utils/mockData';
 
 function Home() {
   return (
---
+++ src/utils/mockData.js
@@ -0,0 +1,27 @@
+export const events = [
+  {
+    groupName: 'Group Name',
+    eventName: 'Event Name',
+    date: new Date(),
+    location: 'san jose, CA',
+  },
+  {
+    groupName: 'Group Name',
+    eventName: 'Event Name',
+    date: new Date(),
+    location: 'san jose, CA',
+  },
+];
+
+export const groups = [
+  {
+    groupName: 'Group Name',
+    users: ['Sam Johnson', 'Josie Rein', 'Jane Doe', 'Sellie Nosh'],
+    description: 'description',
+  },
+  {
+    groupName: 'Group Name',
+    users: ['Sam Johnson', 'Josie Rein', 'Jane Doe'],
+    description: 'description',
+  },
+];
